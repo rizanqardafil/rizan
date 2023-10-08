ab5ef2c9c0dc40d45fe4c672b88000a65b59d535
Rizan Qardafil <rizanqardafil14@gmail.com>
2023-10-08 23:30:29 +0700

    change name website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,24 @@
-import "@/styles/global.css";
-import { useEffect } from "react";
-import type { AppProps } from "next/app";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/Footer";
-import { ThemeProvider } from "next-themes";
-import "nprogress/nprogress.css";
-import NProgress from "nprogress";
-import Router from "next/router";
-import { DefaultSeo, SocialProfileJsonLd } from "next-seo";
-import config from "site.config";
-import { AnimatePresence, motion } from "framer-motion";
-import { usePanelbear } from "@/lib/usePanelBear";
+import '@/styles/global.css'
+import { useEffect } from 'react'
+import type { AppProps } from 'next/app'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/Footer'
+import { ThemeProvider } from 'next-themes'
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import { DefaultSeo, SocialProfileJsonLd } from 'next-seo'
+import config from 'site.config'
+import { AnimatePresence, motion } from 'framer-motion'
+import { usePanelbear } from '@/lib/usePanelBear'
 
-NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  usePanelbear(config.panelbear);
+  usePanelbear(config.panelbear)
   useEffect(() => {
     //CAT
     console.log(
@@ -31,17 +31,17 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         ( (  )   (  ) ) \r
        (__(__)___(__)__)
     `,
-      "font-weight:bold; font-size:15px;color:#ff4747"
-    );
-  }, []);
+      'font-weight:bold; font-size:15px;color:#ff4747'
+    )
+  }, [])
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
+    <ThemeProvider attribute='class' enableSystem={false}>
       <DefaultSeo
         description={config.description}
-        defaultTitle="Hi i'm Wisesa."
-        titleTemplate={`%s · Wisesa.dev`}
+        defaultTitle="Hi i'm Rizan."
+        titleTemplate={`%s · Rizan.dev`}
         openGraph={{
-          title: "Hi i'm Wisesa.",
+          title: "Hi i'm Rizan.",
           description: config.description,
           images: [
             {
@@ -50,30 +50,30 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           ],
         }}
         twitter={{
-          handle: "@svspcs",
-          cardType: "summary_large_image",
+          handle: '@svspcs',
+          cardType: 'summary_large_image',
         }}
       />
       <SocialProfileJsonLd
-        type="Person"
-        name="Anvaqta Tangguh Wisesa"
+        type='Person'
+        name='Anvaqta Tangguh Wisesa'
         url={config.baseUrl}
         sameAs={[
-          "http://www.linkedin.com/in/anvaqta",
-          "https://github.com/kelilipan",
-          "https://svspicious.medium.com/",
-          "https://instagram.com/anvaqta",
+          'http://www.linkedin.com/in/anvaqta',
+          'https://github.com/kelilipan',
+          'https://svspicious.medium.com/',
+          'https://instagram.com/anvaqta',
         ]}
       />
-      <div className="flex flex-col min-h-screen">
+      <div className='flex flex-col min-h-screen'>
         <Navbar />
         <AnimatePresence exitBeforeEnter>
           <motion.main
             key={router.route}
-            className="flex flex-col h-full flex-grow"
-            animate="enter"
-            exit="exit"
-            initial="initial"
+            className='flex flex-col h-full flex-grow'
+            animate='enter'
+            exit='exit'
+            initial='initial'
             variants={{
               initial: { opacity: 0, y: 5 },
               enter: { opacity: 1, y: 0, transition: { duration: 0.1 } },
@@ -86,6 +86,6 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         <Footer />
       </div>
     </ThemeProvider>
-  );
+  )
 }
-export default MyApp;
+export default MyApp
